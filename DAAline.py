17105466202dde3f314ff7c0ab8b03262e1aee7d
dipesh19322 <91831726+dipesh19322@users.noreply.github.com>
2021-10-03 23:08:52 +0530

    Add files via upload

---
+++ DAAline.py
@@ -0,0 +1,37 @@
+import sys
+import pygame
+from pygame import gfxdraw
+
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+screen.fill((0, 0, 0))
+pygame.display.flip()
+
+white = (255, 255, 255)
+
+
+def ROUND(n):
+	return int(n+0.5)
+
+
+def line(x1, y1, x2, y2):
+	x, y = x1, y1
+	length = (x2-x1) if (x2-x1) > (y2-y1) else (y2-y1)
+	dx = (x2-x1)/float(length)
+	dy = (y2-y1)/float(length)
+	gfxdraw.pixel(screen, ROUND(x), ROUND(y), white)
+
+	for i in range(length):
+		x += dx
+		y += dy
+		gfxdraw.pixel(screen, ROUND(x), ROUND(y), white)
+	pygame.display.flip()
+
+
+line(10, 10, 50, 50)
+
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+input()
